76970e2b10b18e6994162c18e7b6d61194cac13b
Thibault Brillet <thbrillet@gmail.com>
2018-08-08 14:21:54 +0200

    CRUD ok pour restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+Restaurant.create(name: "Café Charbon", address: "44000 Nantes", phone_number: "02 41 40 35 94", category: "french")
+Restaurant.create(name: "Mac Donalds Commerce", address: "Place du Commerce, Nantes", phone_number: "", category: "french")
+Restaurant.create(name: "Pizzeria Del Arte", address: "Bouffay, Nantes", phone_number: "02 02 14 65 98", category: "italian")
+Restaurant.create(name: "Le Song", address: "rue de la Marne, 44000 Nantes", phone_number: "02 54 11 12 69", category: "japanese")
+Restaurant.create(name: "Bo Bun", address: "44000 Nantes", phone_number: "", category: "chinese")
